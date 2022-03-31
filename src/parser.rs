84ec57e31af84cb07dcbc5961ac0981ee7ca4f82
Maneren <49210777+Maneren@users.noreply.github.com>
2022-03-31 11:49:34 +0200

    optimize parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,43 +1,49 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Op {
     CellInc, CellDec,
     PtrInc, PtrDec,
     Print, Read,
     BlkPsh, BlkPop,
+    Invalid
 }
 
-pub fn parse(string: String) -> Vec<Op> {
-    let mut ops = Vec::new();
+impl Op {
+    fn is_valid(&self) -> bool {
+        self != &Op::Invalid
+    }
+}
+
+impl From<char> for Op {
+    fn from(ch: char) -> Self {
+        match ch {
+            '+' => Op::CellInc,
+            '-' => Op::CellDec,
+            '>' => Op::PtrInc,
+            '<' => Op::PtrDec,
+            '.' => Op::Print,
+            ',' => Op::Read,
+            '[' => Op::BlkPsh,
+            ']' => Op::BlkPop,
+            _ => Op::Invalid,
+        }
+    }
+}
+
+pub fn parse(string: &str) -> Vec<Op> {
     string
         .chars()
-        .for_each(|char| {
-            let op = match char {
-                '+' => Some(Op::CellInc),
-                '-' => Some(Op::CellDec),
-                '>' => Some(Op::PtrInc),
-                '<' => Some(Op::PtrDec),
-                '.' => Some(Op::Print),
-                ',' => Some(Op::Read),
-                '[' => Some(Op::BlkPsh),
-                ']' => Some(Op::BlkPop),
-                _ => None,
-            };
-
-            if let Some(op) = op { ops.push(op); }
-        });
-
-    ops
+        .map(Op::from)
+        .filter(Op::is_valid)
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
-
     #[test]
     fn parse_simple() {
         let program = "+[------->++<]>--.+++.---.";
-        let parsed = super::parse(program.to_owned());
+        let parsed = super::parse(program);
 
         println!("{:?}", parsed);
-
     }
 }
